3ba7ee1277dcbf2e62d1c5178ffc8a64b9d99fdc
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-18 15:29:34 +0600

    funtion created for electionResult

--- problem.js
+++ problem.js
@@ -11,9 +11,39 @@ function validEmail(email) {
         return "Invalid";
     }
     if (email.charAt(0) !== "." && email.charAt(0) !== "-" && email.charAt(0) !== "+" && email.charAt(0) !== "@") {
-        if (email.includes(".com")  && email.includes("@") && !email.includes(" ") && email.endsWith(".com")) {
+        if (email.includes(".com") && email.includes("@") && !email.includes(" ") && email.endsWith(".com")) {
             return true;
         }
     }
     return false;
 }
+
+
+function electionResult(votes) {
+    if (!Array.isArray(votes)) {
+        return "Invalid";
+    }
+    if (votes.length == 0) {
+        return "Draw";
+    }
+    let mangoParty = 0;
+    let bananaParty = 0;
+    for (const party of votes) {
+        if (party === "mango" || party === "Mango") {
+            mangoParty++;
+        } else if(party === "banana" || party === "Banana") {
+            bananaParty++;
+        }
+    }
+    if (mangoParty > bananaParty) {
+        return "Mango";
+    } else if (bananaParty > mangoParty) {
+        return "Banana";
+    }else if (mangoParty == bananaParty) {
+        return "Draw";
+    }
+    else {
+        return "Draw";
+    }
+}
+
