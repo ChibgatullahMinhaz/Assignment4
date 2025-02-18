94753be705d9cc5a144c488a7546e839a6f2ddeb
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-18 20:06:01 +0600

    calculate time

--- problem.js
+++ problem.js
@@ -31,7 +31,7 @@ function electionResult(votes) {
     for (const party of votes) {
         if (party === "mango" || party === "Mango") {
             mangoParty++;
-        } else if(party === "banana" || party === "Banana") {
+        } else if (party === "banana" || party === "Banana") {
             bananaParty++;
         }
     }
@@ -39,7 +39,7 @@ function electionResult(votes) {
         return "Mango";
     } else if (bananaParty > mangoParty) {
         return "Banana";
-    }else if (mangoParty == bananaParty) {
+    } else if (mangoParty == bananaParty) {
         return "Draw";
     }
     else {
@@ -47,18 +47,37 @@ function electionResult(votes) {
     }
 }
 
-function  isBestFriend( f1 , f2 ) {
-   if (typeof f1 !== "object" || typeof f2 !== "object") {
-     return "Invalid";
-   }else if (Array.isArray(f1) && Array.isArray(f2)) {
-    return "Invalid";
-   }
-   if ( typeof f1.name  !== "string" || typeof f1.roll !== "number" || typeof f1.bestFriend !== "number" || typeof f2.name !== "string" || typeof f2.roll !== "number" || typeof f2.bestFriend !== "number") {
-    return "Invalid";
-   }
-   if (f1.roll === f2.bestFriend && f2.roll === f1.bestFriend) {
-    return true
-   }else{
-    return false;
-   }
+function isBestFriend(f1, f2) {
+    if (typeof f1 !== "object" || typeof f2 !== "object") {
+        return "Invalid";
+    } else if (Array.isArray(f1) && Array.isArray(f2)) {
+        return "Invalid";
+    }
+    if (typeof f1.name !== "string" || typeof f1.roll !== "number" || typeof f1.bestFriend !== "number" || typeof f2.name !== "string" || typeof f2.roll !== "number" || typeof f2.bestFriend !== "number") {
+        return "Invalid";
+    }
+    if (f1.roll === f2.bestFriend && f2.roll === f1.bestFriend) {
+        return true
+    } else {
+        return false;
+    }
+}
+
+function calculateWatchTime(times) {
+    if (!Array.isArray(times) || typeof times !== "number") {
+        return "Invalid";
+    }
+    let totalTimes = 0;
+    for (const time of times) {
+        totalTimes = totalTimes + time;
+    }
+    const minute = Math.floor(totalTimes / 60);
+    const remainingMint = Math.floor(minute % 60);
+    const RemeiningSecond = Math.floor(totalTimes % 60);
+    const hour = Math.floor(minute / 60);
+    return {
+        hour: hour,
+        minute: remainingMint,
+        second: RemeiningSecond
+    };
 }
