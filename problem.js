3f8a090a2e082fffaed8b8955d2d29586ab35858
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-18 13:02:05 +0600

    create funtion for cashOut charge

---
+++ problem.js
@@ -0,0 +1,8 @@
+function cashOut( money ) {
+    if ( typeof money !== "number" || money < 0 ) {
+        return "Invalid";
+    }
+    const charge = money * 1.75 / 100;
+    return charge;
+}
+
--- problem1.js
+++
@@ -1,3 +0,0 @@
-function cashOut( money ) {
-    // You have to write your code here
-}
