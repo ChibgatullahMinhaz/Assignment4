3b5446a5ccc74551b4b9defa485f7a0911bba9e2
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-02-18 13:54:19 +0600

    email validation in js

--- problem.js
+++ problem.js
@@ -1,8 +1,19 @@
-function cashOut( money ) {
-    if ( typeof money !== "number" || money < 0 ) {
+function cashOut(money) {
+    if (typeof money !== "number" || money < 0) {
         return "Invalid";
     }
     const charge = money * 1.75 / 100;
     return charge;
 }
 
+function validEmail(email) {
+    if (typeof email !== "string") {
+        return "Invalid";
+    }
+    if (email.charAt(0) !== "." && email.charAt(0) !== "-" && email.charAt(0) !== "+" && email.charAt(0) !== "@") {
+        if (email.includes(".com")  && email.includes("@") && !email.includes(" ") && email.endsWith(".com")) {
+            return true;
+        }
+    }
+    return false;
+}
